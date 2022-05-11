eadcc0f12a2b8185351c2dcb50a6c603e1c79069
Silence <silence1874@foxmail.com>
2022-05-11 14:08:19 +0800

    Admin about view

--- front/src/admin/About.vue
+++ front/src/admin/About.vue
@@ -1,12 +1,44 @@
 <template>
     <div>
-        关于我开发中
+        <el-form :model="form" :rules="formRules" ref="formRef" label-position="top">
+            <el-form-item label="标题" prop="title" style="width: 50%">
+                <el-input v-model="form.title" placeholder="请输入标题"></el-input>
+            </el-form-item>
+
+            <el-form-item label="正文" prop="content">
+                <mavon-editor v-model="form.content"/>
+            </el-form-item>
+
+            <el-form-item style="text-align: right;">
+                <el-button type="primary" icon="el-icon-check" @click="submit">保存</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 
 <script>
     export default {
-        name: "About"
+        name: "About",
+        data() {
+            return {
+                form: {
+                    title: '',
+                    content: '',
+                },
+                formRules: {
+                    title: [{required: true, message: '请输入标题', trigger: 'change'}],
+                }
+            }
+        },
+        created() {
+            this.getData()
+        },
+        methods: {
+            getData() {
+            },
+            submit() {
+            }
+        }
     }
 </script>
 
--- front/src/main.js
+++ front/src/main.js
@@ -5,9 +5,12 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(mavonEditor)
 
 Vue.prototype.$axios = axios
 
