3ccb8627edb5aec85a533502939a723c885e57d6
Silence <silence1874@foxmail.com>
2022-05-11 21:21:46 +0800

    refactor: Axios baseUrl

--- front/src/main.js
+++ front/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
+import './axios.js'
 import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
 
