7a0bfbc8c916b6b45f1bdb89334eba44dd76dd07
Silence <silence1874@foxmail.com>
2022-05-12 20:33:13 +0800

    feat: Admin tag add api

--- back/src/main/java/top/silence/controller/admin/AdminTagController.java
+++ back/src/main/java/top/silence/controller/admin/AdminTagController.java
@@ -2,10 +2,7 @@ package top.silence.controller.admin;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import top.silence.dto.Result;
 import top.silence.entity.TagDO;
 import top.silence.service.TagService;
@@ -21,6 +18,13 @@ public class AdminTagController {
     private Result listTag(@PathVariable Integer pageNum, @PathVariable Integer pageSize) {
         Page<TagDO> pages = tagService.listTag(pageNum, pageSize);
         return Result.ok("成功获得标签分页信息", pages);
+    }
+
+    @PostMapping("/tag")
+    private Result addTag(@RequestBody TagDO tagDO) {
+        tagService.save(tagDO);
+        Long newId = tagDO.getId();
+        return Result.ok("成功添加标签", newId);
     }
 
 }
