8216ba6d502863ad8c0f486ef83117d7fc3d25bf
Silence <silence1874@foxmail.com>
2022-05-16 13:25:46 +0800

    feat: Scroll to top

--- front/src/main.js
+++ front/src/main.js
@@ -30,6 +30,27 @@ Vue.prototype.msgError = function (msg) {
   this.$message.error(msg)
 }
 
+//滚动至页面顶部
+const cubic = value => Math.pow(value, 3);
+const easeInOutCubic = value => value < 0.5 ? cubic(value * 2) / 2 : 1 - cubic((1 - value) * 2) / 2;
+Vue.prototype.scrollToTop = function () {
+  const el = document.documentElement
+  const beginTime = Date.now()
+  const beginValue = el.scrollTop
+  const rAF = window.requestAnimationFrame || (func => setTimeout(func, 16))
+  const frameFunc = () => {
+    const progress = (Date.now() - beginTime) / 500;
+    if (progress < 1) {
+      el.scrollTop = beginValue * (1 - easeInOutCubic(progress))
+      rAF(frameFunc)
+    } else {
+      el.scrollTop = 0
+    }
+  }
+  rAF(frameFunc)
+}
+
+
 new Vue({
   router,
   store,
