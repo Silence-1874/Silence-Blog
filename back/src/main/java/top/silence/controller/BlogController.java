fcc7052a9ac0602b7e80a6ef1229602f9023080f
Silence <silence1874@foxmail.com>
2022-08-20 19:15:25 +0800

    refactor: Rename class(typo)

--- back/src/main/java/top/silence/controller/BlogController.java
+++ back/src/main/java/top/silence/controller/BlogController.java
@@ -13,7 +13,7 @@ import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
-public class BlogCotroller {
+public class BlogController {
 
     @Autowired
     private BlogService blogService;
