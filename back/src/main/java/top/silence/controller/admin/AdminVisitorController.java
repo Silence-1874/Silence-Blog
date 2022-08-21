757889f52dd9a4a595e4eeb99d0743d997fcfe7e
Silence <silence1874@foxmail.com>
2022-08-21 13:12:20 +0800

    feat: VisitorLog order by first_time desc

--- back/src/main/java/top/silence/controller/admin/AdminVisitorController.java
+++ back/src/main/java/top/silence/controller/admin/AdminVisitorController.java
@@ -1,6 +1,7 @@
 package top.silence.controller.admin;
 
 import cn.dev33.satoken.annotation.SaCheckLogin;
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ public class AdminVisitorController {
     @GetMapping("/visitor/{pageNum}/{pageSize}")
     public Result page(@PathVariable("pageNum") Integer pageNum, @PathVariable("pageSize") Integer pageSize) {
         Page<VisitorDO> page = new Page<>(pageNum, pageSize);
+        page.addOrder(OrderItem.desc("first_time"));
         return Result.ok("成功获得访客列表", visitorService.page(page));
     }
 
